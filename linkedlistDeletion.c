7ea59f5a95a4680cbc017ba8e54877f57d585dcf
guru-dharshan <dharshangd1906@gmail.com>
2021-12-13 22:01:11 +0530

    linkedlist-deletion

--- linkedlistDeletion.c
+++ linkedlistDeletion.c
@@ -20,14 +20,33 @@ void insert(int x,int p){
         head=temp;
     }
     }
-    struct Node* temp1 = head;
+    else{
+  struct Node* temp1 = head;
     for(int i=0;i<p-2;i++){
         temp1=temp1->next;
     }
     temp->next=temp1->next;
     temp1->next=temp;
+    }
+  
     
     
+}
+
+void delete(int p){
+    struct Node* temp = head;
+    if(p==1){
+        head=head->next;
+        free(temp);
+    }
+    else{
+        for(int i=0;i<p-2;i++){
+            temp=temp->next;
+        }
+        struct Node* temp1 =temp->next;
+        temp->next=temp1->next;
+        free(temp1);
+    }
 }
 
 void printlist(){
@@ -37,6 +56,7 @@ void printlist(){
         printf("%d",temp->data);
         temp = temp->next;
     }
+    printf("\n");
 }
 int main()
 {
@@ -45,7 +65,15 @@ int main()
     int n,x;
     
         insert(1,1);
-        insert(3,2);
         insert(2,2);
+        insert(3,3);
+        insert(4,4);
+        insert(5,5);
        printlist();
+       delete(2);
+       printlist();
+
+
+
+
 }
