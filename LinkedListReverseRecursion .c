2c87f2524d9ddfbd93d39b9e810a22233dca89e1
guru-dharshan <dharshangd1906@gmail.com>
2021-12-19 00:41:07 +0530

    reverse a linked list using recursion

--- LinkedListReverseRecursion .c
+++ LinkedListReverseRecursion .c
@@ -8,7 +8,6 @@ struct Node{
 };
 struct Node* head;
 void insert(int x,int p){
-   
     struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
     temp->data=x;
     if(p==1){
@@ -22,52 +21,55 @@ void insert(int x,int p){
     }
     }
     else{
-    struct Node* temp1 = head;
+ struct Node* temp1 = head;
     for(int i=0;i<p-2;i++){
         temp1=temp1->next;
     }
     temp->next=temp1->next;
     temp1->next=temp;
+    
     }
    
-   
+    
 }
 
 void printlist(){
-   
      struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
      temp=head;
     while(temp!=NULL){
         printf("%d",temp->data);
         temp = temp->next;
     }
+    printf("\n");
 }
-void reverse(){
-   
-    struct Node* p=NULL;
-    struct Node* c=head;
-    struct Node* n=c->next;
-   
-    while(c!=NULL){
-        n=c->next;
-        c->next=p;
-        p=c;
-        c=n;
+
+void reverse(struct Node* c){
+    
+    static struct Node* p=NULL;
+    static struct Node* n=NULL;
+    if(c==NULL){
+        head=p;
+        return;
     }
-    head=p;
-   
+    n=c->next;
+    c->next=p;
+    p=c;
+    c=n;
+    reverse(c);
+
 }
 int main()
 {
     head=(struct Node*)malloc(sizeof(struct Node));
     head=NULL;
     int n,x;
-   
+    
         insert(1,1);
         insert(2,2);
         insert(3,3);
         insert(4,4);
+        insert(5,5);
        printlist();
-       reverse();
+       reverse(head);
        printlist();
 }
